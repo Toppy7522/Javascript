3ee968a16de3b19fdda9ce91f740295e88d8f534
Vaibav Vaidyanathan <117505194+Toppy7522@users.noreply.github.com>
2023-03-05 18:04:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+/*
+
+
+let count1 = document.getElementById('count-el')
+
+
+let count = 0
+
+function increment(){ 
+    count = count + 1
+    count1.innerText = count
+}
+function save(){
+    console.log(count)
+}
+
+*/
+/*
+let username = "per"
+let message = 'You have three new notifications'
+
+
+
+let messagetoUser = message + ", " + username + "!"
+console.log(messagetoUser)
+*/
+let name = 42
+let greeting = "Hi, my name is "
+let myGreeting = greeting + name
+
+//console.log(myGreeting)
+
+let points = 4
+let bonusPoints = " 10"
+
+let total = points + bonusPoints
+console.log(total)
