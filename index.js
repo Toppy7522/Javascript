ee2456854d9b7b72c6e1eb01bd5945ee4b06f01a
Vaibav Vaidyanathan <117505194+Toppy7522@users.noreply.github.com>
2023-03-05 19:54:18 +0530

    Passenger Counting App completed

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
 
-/*
+
 
 
 let count1 = document.getElementById('count-el')
-
-
+let valueEl = document.getElementById('value-el')
 let count = 0
 
 function increment(){ 
-    count = count + 1
-    count1.innerText = count
+    count += 1
+    count1.textContent = count
 }
 function save(){
-    console.log(count)
+    let countStr = count + " - " 
+    valueEl.textContent +=  countStr
+    count1.textContent = 0
+    count = 0
+    
+
 }
 
-*/
-/*
-let username = "per"
-let message = 'You have three new notifications'
-
-
-
-let messagetoUser = message + ", " + username + "!"
-console.log(messagetoUser)
-*/
-let name = 42
-let greeting = "Hi, my name is "
-let myGreeting = greeting + name
-
-//console.log(myGreeting)
-
-let points = 4
-let bonusPoints = " 10"
-
-let total = points + bonusPoints
-console.log(total)
